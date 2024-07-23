6f9a2575f0d91bfd22590548161575d241cf23da
Selorm <ranselorm@outlook.com>
2024-07-23 21:57:44 +0000

    Onboarding img

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,7 @@
+import PatientForm from "@/components/forms/PatientForm";
 import { Button } from "@/components/ui/button";
 import Image from "next/image";
+import Link from "next/link";
 
 export default function Home() {
   return (
@@ -13,8 +15,26 @@ export default function Home() {
             width={1000}
             className="w-fit h-10 mb-12"
           />
+          <PatientForm />
+          <div className="text-14-regular mt-20 flex justify-between">
+            <p className="justify-items-end text-dark-600 xl:text-left">
+              © 2024 CarePulse
+            </p>
+            <Link href="/?admin=true" className="text-green-500">
+              Admin
+            </Link>
+          </div>
         </div>
       </section>
+      <div>
+        <Image
+          src="/assets/images/onboarding-img.png"
+          alt="patient"
+          width={1000}
+          height={1000}
+          className="side-img max-w-[50%]"
+        />
+      </div>
     </div>
   );
 }
