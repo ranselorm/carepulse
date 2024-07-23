bb6d3040d3fd845610566d23d8de140488af1adf
Selorm <ranselorm@outlook.com>
2024-07-23 21:43:16 +0000

    Add logo

--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,20 @@
 import { Button } from "@/components/ui/button";
+import Image from "next/image";
 
 export default function Home() {
   return (
-    <div>
-      <h3 className="text-white">Home</h3>
-      <Button>Let's Go</Button>
+    <div className="flex h-screen max-h-screen">
+      <section className="remove-scrollbar container my-auto">
+        <div className="sub-container max-w-[496px]">
+          <Image
+            src="/assets/icons/logo-full.svg"
+            alt=""
+            height={1000}
+            width={1000}
+            className="w-fit h-10 mb-12"
+          />
+        </div>
+      </section>
     </div>
   );
 }
